1ca15b6c67897e9d7f2b804b0968e3bf0eb9ae8a
Ярослав Пономарёв <yaroslavponomarev.pro@gmail.com>
2024-09-29 16:43:01 +0400

    Com

---
+++ Messanger.ConsoleClient/Message.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messanger.ConsoleClient
+{
+    [Serializable]
+    public class Message
+    {
+        public string UserName { get; set; }
+        public string MessageText { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        public Message(string userName, string messageText, DateTime timeStamp)
+        {
+            UserName = userName;
+            MessageText = messageText;
+            TimeStamp = timeStamp;
+        }
+
+        public Message()
+        {
+            UserName = "yarponomarev";
+            MessageText = "Server is running...";
+            TimeStamp = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            string output = string.Format("{0} <{2}>: {1}", UserName, MessageText, TimeStamp);
+            return output;
+        }
+    }
+}
---
+++ Messanger.ConsoleClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Messanger.ConsoleClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Message message = new Message("Yaroslav", "Hello World!", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(message);
+            Console.WriteLine(output);
+
+            Message deserializedMessage = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMessage);
+
+            // {"UserName":"Yaroslav","MessageText":"Hello World!","TimeStamp":"2024-09-29T09:50:31.7364167Z"}
+            // Yaroslav <29.09.2024 9:50:31>: Hello World!
+        }
+    }
+}
